d61fc4ed35cbcb9451c2ad298ed2952bc66179ff
kobayashi <syosinekakisan@gmail.com>
2020-12-26 21:10:28 +0900

    Added Backup & anyone sed path-rewrite.sh

--- path-rewrite.sh
+++ path-rewrite.sh
@@ -6,13 +6,25 @@ today=$(date "+%Y%m%d")
 yesterday=$(date --date "1 day ago" "+%Y%m%d")
 
 ### directory of motion.conf
-dir=/home/camera/
+picture_dir=/var/motion-log/picture
+#movie_dir=/var/motion-log/movie
 
 ### Create Folder of today
-mkdir -p ${dir}${today}
+mkdir -p ${picture_dir}${today}
+
+### Backup original file
+if [[ -e /etc/motion/motion.conf ]]; then
+  cp /etc/motion/motion.conf /etc/motion/motion.conf.old
+fi
 
 ### Rewrite of motion.conf
+sed -i -e 's/daemon off/deamon on/g' /etc/motion/motion.conf
 sed -i -e 's/'$yesterday'/'$today'/g' /etc/motion/motion.conf
+sed -i -e 's/picture_output off/picture_output on/g' /etc/motion/motion.conf
+sed -i -e 's/movie_output on/movie_output off/g' /etc/motion/motion.conf
+sed -i -e 's/movie_codec mkv/movie_codec mp4/g' /etc/motion/motion.conf
+sed -i -e 's/threshold 1500/threshold 1600/g' /etc/motion/motion.conf
+sed -i -e 's/stream_localhost off/stream_localhost on/g' /etc/motion/motion.conf
 
 ### Reload motion.conf at kill&<PID>
 motion_pid=$(ps -ef | pgrep motion)
